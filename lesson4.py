2b579e00523f40d691d747df993b8890e24fe9a6
BDOlegsey <81856546+BDOlegsey@users.noreply.github.com>
2024-11-06 19:32:53 +0300

    Update lesson4.py

--- lesson4.py
+++ lesson4.py
@@ -2,7 +2,27 @@ import pygame
 import time
 from random import randint
 
-R = 35
+sizeX, sizeY = 1000, 500
+
+
+class Ball:
+    def __init__(self, x, y, R, speedX, speedY):
+        self.x = x
+        self.y = y
+        self.speedX = speedX
+        self.speedY = speedY
+        self.R = R
+
+    def move(self):
+        self.x += self.speedX
+        self.y += self.speedY
+
+    def border(self):
+        if self.x - self.R <= 0 or self.x + self.R >= sizeX:
+            self.x = 500
+            self.y = randint(50, 450)
+        elif self.y - self.R <= 0 or self.y + self.R >= sizeY:
+            self.speedY *= -1
 
 
 class Player:
@@ -12,53 +32,22 @@ class Player:
         self.sizeX = sizeX
         self.sizeY = sizeY
 
-    def face(self, x, y, vx, turn):
+    def face(self, ball, turn):
         if turn == 'left':
-            if x - R <= self.x + 30:
-                if y >= self.y and y <= self.y + 100:
-                    return -vx
+            if ball.x - ball.R <= self.x + 30:
+                if ball.y >= self.y and ball.y <= self.y + 100:
+                    return -ball.speedX
         else:
-            if x + R >= self.x:
-                if y >= self.y and y <= self.y + 100:
-                    return -vx
-        return vx
+            if ball.x + ball.R >= self.x:
+                if ball.y >= self.y and ball.y <= self.y + 100:
+                    return -ball.speedX
+        return ball.speedX
 
-    def move_bot(self, y):
-        if self.y + 50 < y - 50:
+    def move_bot(self, ball):
+        if self.y + 50 < ball.y - 50:
             self.y += 2
-        elif self.y + 50 > y + 50:
+        elif self.y + 50 > ball.y + 50:
             self.y -= 2
-
-
-sizeX, sizeY = 1000, 500
-
-def border(X, Y, vx, vy):
-    if X - R <= 0:
-        vx = -vx
-    elif X + R >= sizeX:
-        vx = -vx
-    elif Y - R <= 0:
-        vy = -vy
-    elif Y + R >= sizeY:
-        vy = -vy
-
-    return [vx, vy]
-
-def border_key(X, Y, g):
-    if X - R <= 0:
-        X = R
-    if X + R >= sizeX:
-        X = sizeX - R
-    if Y - R <= 0:
-        Y = R
-    if Y + R >= sizeY:
-        Y = sizeY - R
-        g = 0
-
-    return [X, Y, g]
-
-def dist(X, Y, X1, Y1):
-    return ((X - X1)**2 + (Y - Y1)**2)**0.5
 
 
 pygame.init()
@@ -75,21 +64,7 @@ FPS = 60
 player = Player(15, 20)
 player_bot = Player(955, 20)
 
-X, Y = 200, 200
-
-vx = 10; vy = 4
-vx1 = 10; vy1 = 2
-run_hor = 0
-run_vert = 0
-k = 1
-
-g = 4
-
-
-is_draw = False
-
-v1 = 0
-
+ball = Ball(200, 200, 35, 10, 4)
 
 while game_run:
     clock.tick(FPS)
@@ -106,26 +81,18 @@ while game_run:
         if event.type == pygame.MOUSEBUTTONUP:
             pass
 
-    X += vx
-    Y += vy
+    ball.move()
+    ball.border()
 
-    vx, vy = border(X, Y, vx, vy)
+    ball.speedX = player.face(ball, 'left')
+    ball.speedX = player_bot.face(ball, 'right')
 
-    vx = player.face(X, Y, vx, 'left')
-    vx = player_bot.face(X, Y, vx, 'right')
 
-    if X - R <= 0:
-        X = 500
-        Y = randint(50, 450)
-    if X + R >= 1000:
-        X = 500
-        Y = randint(50, 450)
-
-    player_bot.move_bot(Y)
+    player_bot.move_bot(ball)
 
     pygame.draw.rect(screen, [0, 0, 0], [player.x, player.y, player.sizeX, player.sizeY])
     pygame.draw.rect(screen, [0, 0, 0], [player_bot.x, player_bot.y, player_bot.sizeX, player_bot.sizeY])
-    pygame.draw.circle(screen, [0, 0, 0], [X, Y], R)
+    pygame.draw.circle(screen, [0, 0, 0], [ball.x, ball.y], ball.R)
 
     pygame.display.flip()
 
@@ -133,13 +100,5 @@ while game_run:
 screen.fill((255, 255, 255))
 pygame.display.flip()
 
-
-
-# screen.fill((0, 0, 255))
-
-# pygame.draw.rect(screen, [0, 0, 0], [20, 20, 100, 100], 2, 10)
-# pygame.draw.circle(screen, [0, 0, 0], [200, 300], 100)
-
-# pygame.display.flip()
 
 pygame.quit()
