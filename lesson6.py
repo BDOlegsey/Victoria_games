16f106cbdce3295197fd4cc09a617f78776acdcb
BDOlegsey <81856546+BDOlegsey@users.noreply.github.com>
2024-11-20 19:06:04 +0300

    Update lesson6.py

--- lesson6.py
+++ lesson6.py
@@ -72,6 +72,21 @@ class Person:
             _screen.blit(self.images[1], (self.x, self.y))
 
 
+class Button:
+    def __init__(self, x, y, sizeX, sizeY, text, color):
+        self.x = x
+        self.y = y
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.text = text
+        self.color = color
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, [self.x, self.y, self.sizeX, self.sizeY])
+        _text = my_font.render(self.text, True, (255, 255, 255))
+        screen.blit(_text, (self.x + self.sizeX//3, self.y + self.sizeY//3))
+
+
 pygame.init()
 
 screen = pygame.display.set_mode([sizeX, sizeY])
@@ -83,6 +98,8 @@ clock = pygame.time.Clock()
 game_run = True
 FPS = 60
 count = 0
+
+turn = 'start'
 
 pygame.mixer.init()
 pygame.mixer.music.load('snd\\test.mp3')
@@ -97,7 +114,7 @@ person_img_right = pygame.image.load('img\\person_right.png')
 ball_img = pygame.image.load('img\\ball.png')
 player_img = pygame.image.load('img\\player.png')
 
-my_font = pygame.font.SysFont('arial', 30)
+my_font = pygame.font.SysFont('arial', 40)
 text1 = my_font.render('Hello world!', True, (0, 0, 0))
 text2 = my_font.render(str(count), True, (0, 0, 0))
 
@@ -107,59 +124,80 @@ player_bot = Player(800, 20, player_img)
 
 ball = Ball(200, 200, 100, 10, 4, ball_img)
 
-# Нормальный пинг понг
-# Марио
-# Звёздные войны
+start_button = Button(400, 200, 300, 70, 'START', [0, 0, 0])
+back_button = Button(400, 20, 300, 70, 'BACK', [0, 0, 0])
 
 while game_run:
-    clock.tick(FPS)
-    screen.fill((255, 255, 255))
+    if turn == 'start':
+        screen.fill((255, 255, 255))
+        start_button.draw()
+        pygame.display.flip()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 4:
-                player.y -= 10
-                person.x += 10
-                person.right = True
-            if event.button == 5:
-                player.y += 10
-                person.x -= 10
-                person.right = False
-            if event.button == 1:
-                if is_music:
-                    pygame.mixer.music.pause()
-                    is_music = False
-                else:
-                    pygame.mixer.music.unpause()
-                    is_music = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    _x = pygame.mouse.get_pos()[0]
+                    _y = pygame.mouse.get_pos()[1]
+                    if _x >= start_button.x and _x <= start_button.x + start_button.sizeX:
+                        if _y >= start_button.y and _y <= start_button.y + start_button.sizeY:
+                            turn = 'game'
+    elif turn == 'game':
+        clock.tick(FPS)
+        screen.fill((255, 255, 255))
+        back_button.draw()
 
-    ball.move()
-    count = ball.border(count)
-    text2 = my_font.render(str(count), True, (0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    player.y -= 10
+                    person.x += 10
+                    person.right = True
+                if event.button == 5:
+                    player.y += 10
+                    person.x -= 10
+                    person.right = False
+                if event.button == 1:
+                    _x = pygame.mouse.get_pos()[0]
+                    _y = pygame.mouse.get_pos()[1]
+                    if _x >= back_button.x and _x <= back_button.x + back_button.sizeX:
+                        if _y >= back_button.y and _y <= back_button.y + back_button.sizeY:
+                            turn = 'start'
+                    if is_music:
+                        pygame.mixer.music.pause()
+                        is_music = False
+                    else:
+                        pygame.mixer.music.unpause()
+                        is_music = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                pass
 
-    ball.speedX = player.face(ball, 'left')
-    ball.speedX = player_bot.face(ball, 'right')
+        ball.move()
+        count = ball.border(count)
+        text2 = my_font.render(str(count), True, (0, 0, 0))
 
-    player_bot.move_bot(ball)
+        ball.speedX = player.face(ball, 'left')
+        ball.speedX = player_bot.face(ball, 'right')
 
-    #pygame.draw.rect(screen, [0, 0, 0], [player.x, player.y, player.sizeX, player.sizeY])
-    #pygame.draw.rect(screen, [0, 0, 0], [player_bot.x, player_bot.y, player_bot.sizeX, player_bot.sizeY])
-    #pygame.draw.circle(screen, [0, 0, 0], [ball.x, ball.y], ball.R)
+        player_bot.move_bot(ball)
 
-    screen.blit(player.image, (player.x, player.y))
-    screen.blit(player_bot.image, (player_bot.x, player_bot.y))
-    screen.blit(ball.image, (ball.x, ball.y))
+        #pygame.draw.rect(screen, [0, 0, 0], [player.x, player.y, player.sizeX, player.sizeY])
+        #pygame.draw.rect(screen, [0, 0, 0], [player_bot.x, player_bot.y, player_bot.sizeX, player_bot.sizeY])
+        #pygame.draw.circle(screen, [0, 0, 0], [ball.x, ball.y], ball.R)
 
-    person.draw(screen)
+        screen.blit(player.image, (player.x, player.y))
+        screen.blit(player_bot.image, (player_bot.x, player_bot.y))
+        screen.blit(ball.image, (ball.x, ball.y))
 
-    screen.blit(text1, (50, 50))
-    screen.blit(text2, (50, 100))
+        person.draw(screen)
 
-    pygame.display.flip()
+        screen.blit(text1, (50, 50))
+        screen.blit(text2, (50, 100))
+
+        pygame.display.flip()
 
 
 screen.fill((255, 255, 255))
